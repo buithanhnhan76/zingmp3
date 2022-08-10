b10fd43ed0258b988b7b0d7aa149f38280d119ca
buithanhnhan76 <nhanbui37@gmail.com>
2022-08-10 13:50:49 +0700

    convert func to arrow func

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import 'normalize.css';
 import Head from 'next/head';
-// import Image from 'next/image';
-import styles from '../styles/Home.module.scss';
 import React from 'react';
+import styles from '../styles/Home.module.scss';
 import Header from '../src/components/header/index';
 import SideBar from '../src/components/sidebar';
 import Gallery from '../src/components/gallery';
@@ -18,7 +17,7 @@ import RadioCarousel from '../src/components/radio-carousel';
 import Event from '../src/components/event';
 import Partner from '../src/components/partner';
 
-export default function Home() {
+function Home() {
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -30,73 +29,27 @@ export default function Home() {
 				/>
 			</Head>
 			<main className={styles['home-main']}>
-				<SideBar></SideBar>
+				<SideBar />
 				<div>
-					<Header></Header>
-					<Gallery></Gallery>
-					<NewRelease></NewRelease>
-					<PlayList></PlayList>
-					<FavoriteArtist></FavoriteArtist>
-					<PlayList></PlayList>
-					<Chart></Chart>
-					<Card></Card>
-					<Carousel></Carousel>
-					<PlayList></PlayList>
-					<NewReleaseSlider></NewReleaseSlider>
-					<PlaylistSlider></PlaylistSlider>
-					<PlayList></PlayList>
-					<RadioCarousel></RadioCarousel>
-					<Event></Event>
-					<Partner></Partner>
+					<Header />
+					<Gallery />
+					<NewRelease />
+					<PlayList />
+					<FavoriteArtist />
+					<PlayList />
+					<Chart />
+					<Card />
+					<Carousel />
+					<PlayList />
+					<NewReleaseSlider />
+					<PlaylistSlider />
+					<PlayList />
+					<RadioCarousel />
+					<Event />
+					<Partner />
 				</div>
 			</main>
-			{/* <main className={styles.main}>
-				<h1 className={styles.title}>
-					Welcome to <a href="https://nextjs.org">Next.js!</a>
-				</h1>
-
-				<p className={styles.description}>
-					Get started by editing <code className={styles.code}>pages/index.js</code>
-				</p>
-
-				<div className={styles.grid}>
-					<a href="https://nextjs.org/docs" className={styles.card}>
-						<h2>Documentation &rarr;</h2>
-						<p>Find in-depth information about Next.js features and API.</p>
-					</a>
-
-					<a href="https://nextjs.org/learn" className={styles.card}>
-						<h2>Learn &rarr;</h2>
-						<p>Learn about Next.js in an interactive course with quizzes!</p>
-					</a>
-
-					<a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
-						<h2>Examples &rarr;</h2>
-						<p>Discover and deploy boilerplate example Next.js projects.</p>
-					</a>
-
-					<a
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-						className={styles.card}
-					>
-						<h2>Deploy &rarr;</h2>
-						<p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-					</a>
-				</div>
-			</main>
-
-			<footer className={styles.footer}>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Powered by{' '}
-					<span className={styles.logo}>
-						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-					</span>
-				</a>
-			</footer> */}
 		</div>
 	);
 }
+export default Home;
