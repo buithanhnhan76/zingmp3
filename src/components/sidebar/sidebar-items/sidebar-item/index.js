c8d8931f33e053148269c7622780dfd1ed8d2fbf
thanhnhan.bui <thanhnhan.bui@m.citynow.vn>
2022-08-11 21:36:59 +0700

    update sidebar

--- src/components/sidebar/sidebar-items/sidebar-item/index.js
+++ src/components/sidebar/sidebar-items/sidebar-item/index.js
@@ -1,15 +1,16 @@
 import { PlayCircleOutlined } from '@ant-design/icons';
 import styles from './sidebar-item.module.scss';
 
-const SideBarItem = (props) =>
-  props.item.title !== '' ? (
+const SideBarItem = ({ item }) =>
+  item.title !== '' ? (
     <div className={styles['sidebar-item-container']}>
       <div className={styles['sidebar-item-content']}>
-        <PlayCircleOutlined /> {props.item.title}
+        <PlayCircleOutlined className={styles['sidebar-item-icon']} />{' '}
+        {item.title}
       </div>
     </div>
   ) : (
-    <div className={styles["sidebar-line-container"]}>
+    <div className={styles['sidebar-line-container']}>
       <hr />
     </div>
   );
