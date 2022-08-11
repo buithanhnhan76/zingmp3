38b52cd9fe9fe4859827c50fa943c4857eabbfee
thanhnhan.bui <thanhnhan.bui@m.citynow.vn>
2022-08-11 21:30:25 +0700

    update sidebar

--- src/components/sidebar/sidebar-banner-items/sidebar-banner-item/index.js
+++ src/components/sidebar/sidebar-banner-items/sidebar-banner-item/index.js
@@ -1,8 +1,25 @@
-import styles from "./sidebar-banner-item.module.scss";
+import styles from './sidebar-banner-item.module.scss';
 
-const SideBarBannerItem = (props) => ( <div className={styles["sidebar-banner-item-container"]}>
-    <h3>{props.item.title}</h3>
-    <button className={props.item.type !== "vip-banner"?styles["sidebar-banner-item-button"]:styles["sidebar-vip-banner-item-button"]}>{props.item.label}</button>
-</div> );
- 
+const SideBarBannerItem = ({ item }) => (
+  <div
+    className={
+      item.type !== 'vip-banner'
+        ? styles['sidebar-banner-item-container']
+        : styles['sidebar-vip-banner-item-container']
+    }
+  >
+    <h3>{item.title}</h3>
+    <button
+      type="button"
+      className={
+        item.type !== 'vip-banner'
+          ? styles['sidebar-banner-item-button']
+          : styles['sidebar-vip-banner-item-button']
+      }
+    >
+      {item.label}
+    </button>
+  </div>
+);
+
 export default SideBarBannerItem;
