299d8e6261de1aba207007a50e5f4fb614b157d8
buithanhnhan76 <nhanbui37@gmail.com>
2022-08-16 14:39:51 +0700

    fix bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import 'normalize.css';
 import Head from 'next/head';
 import React from 'react';
 import useTrans from '../hooks/useStrans';
+import iconZing from '../../public/icons/icon-zing.png';
 
 const Home = () => {
   const trans = useTrans();
@@ -10,10 +11,7 @@ const Home = () => {
       <Head>
         <title>{trans.head.title} </title>
         <meta name="description" content="Zing Mp3" />
-        <link
-          rel="icon"
-          href="https://static-zmp3.zmdcdn.me/skins/zmp3-v5.2/images/icon_zing_mp3_60.png"
-        />
+        <link rel="icon" href={iconZing.src} />
       </Head>
     </div>
   );
