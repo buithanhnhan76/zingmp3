7d8ae7f91abdf72f70fc14e2ffe54bc98ea13b17
THANH NHAN BUI <thanhnhan.bui@m.citynow.vn>
2022-08-17 11:27:25 +0700

    update header

--- src/layouts/Header/mains/HeaderRight/HeaderRightSettingItems/index.jsx
+++ src/layouts/Header/mains/HeaderRight/HeaderRightSettingItems/index.jsx
@@ -3,16 +3,16 @@ import styles from "./HeaderRightSettingItems.module.scss";
 import Clip from "public/icons/clip.svg";
 
 const listSettingItems = [
-  { src: Clip.src },
-  { src: Clip.src },
-  { src: Clip.src },
-  { src: Clip.src },
+  {id: 1, src: Clip.src },
+  {id: 2, src: Clip.src },
+  {id: 3, src: Clip.src },
+  {id: 4, src: Clip.src },
 ];
 
 const HeaderRightSettingItems = () => (
   <div className={styles["header-setting-items-container"]}>
     {listSettingItems.map((item) => (
-      <HeaderRightSettingItem item={item} />
+      <HeaderRightSettingItem item={item} key={item.id}/>
     ))}
   </div>
 );
