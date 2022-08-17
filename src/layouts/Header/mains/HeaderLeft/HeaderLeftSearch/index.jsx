5168947f5b0aa65977119e2df9de3648bc55d452
buithanhnhan76 <nhanbui37@gmail.com>
2022-08-17 11:02:17 +0700

    update header

--- src/layouts/Header/mains/HeaderLeft/HeaderLeftSearch/index.jsx
+++ src/layouts/Header/mains/HeaderLeft/HeaderLeftSearch/index.jsx
@@ -3,7 +3,7 @@ import styles from "./HeaderLeftSearch.module.scss";
 
 const HeaderLeftSearch = () => (
   <div className={styles["header-left-search-container"]}>
-    <SearchOutlined />
+    <SearchOutlined style={{fontSize: '16px', color: '#b3b2b6'}} />
     <input
       placeholder="Tìm kiếm bài hát, nghệ sĩ, lời bài hát..."
       className={styles["header-left-search"]}
