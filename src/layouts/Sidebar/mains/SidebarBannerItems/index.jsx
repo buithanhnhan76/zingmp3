80d8033dca1333f883ccaca4bea467528d20207a
THANH NHAN BUI <thanhnhan.bui@m.citynow.vn>
2022-08-18 08:44:25 +0700

    update sidebar

--- src/layouts/Sidebar/mains/SidebarBannerItems/index.jsx
+++ src/layouts/Sidebar/mains/SidebarBannerItems/index.jsx
@@ -1,12 +1,12 @@
 // f2 component
-import SideBarBannerItem from '../../components/SidebarBannerItem';
+import SidebarBannerItem from '../../components/SidebarBannerItem';
 // style
 import styles from './SidebarBannerItems.module.scss';
 
 const SideBarBannerItems = ({ trans }) => (
   <div className={styles['sidebar-banner-items-container']}>
     {trans.sidebar['banner-items'].map((item) => (
-      <SideBarBannerItem key={item.title} item={item} />
+      <SidebarBannerItem key={item.title} item={item} />
     ))}
   </div>
 );
