b98ae1625a1886a13d89d67ddaaa8afe45670abb
buithanhnhan76 <nhanbui37@gmail.com>
2022-08-18 10:28:23 +0700

    optimize

--- src/layouts/Header/components/HeaderRightLogin/index.jsx
+++ src/layouts/Header/components/HeaderRightLogin/index.jsx
@@ -1,10 +1,15 @@
 import styles from "./HeaderRightLogin.module.scss";
 // next image
-import Image from 'next/image';
+import Image from "next/image";
 
 const HeaderRightLogin = () => (
   <div>
-    <Image className={styles["header-right-login-photo"]} width={40} height={40} src="/photos/user.jpg" />
+    <Image
+      className={styles["header-right-login-photo"]}
+      width={40}
+      height={40}
+      src="/photos/user.jpg"
+    />
   </div>
 );
 
--- src/layouts/Header/components/HeaderRightSettingItems/index.jsx
+++ src/layouts/Header/components/HeaderRightSettingItems/index.jsx
@@ -6,16 +6,16 @@ import styles from "./HeaderRightSettingItems.module.scss";
 import Clip from "public/icons/clip.svg";
 
 const listSettingItems = [
-  {id: 1, src: Clip.src },
-  {id: 2, src: Clip.src },
-  {id: 3, src: Clip.src },
-  {id: 4, src: Clip.src },
+  { id: 1, src: Clip.src },
+  { id: 2, src: Clip.src },
+  { id: 3, src: Clip.src },
+  { id: 4, src: Clip.src },
 ];
 
 const HeaderRightSettingItems = () => (
   <div className={styles["header-setting-items-container"]}>
     {listSettingItems.map((item) => (
-      <HeaderRightSettingItem item={item} key={item.id}/>
+      <HeaderRightSettingItem item={item} key={item.id} />
     ))}
   </div>
 );
