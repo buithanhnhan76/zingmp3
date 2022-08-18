c8bf69b192002f4bea088f9c49371adb4e2c045c
THANH NHAN BUI <thanhnhan.bui@m.citynow.vn>
2022-08-18 09:00:00 +0700

    update sidebar

--- src/layouts/Sidebar/index.jsx
+++ src/layouts/Sidebar/index.jsx
@@ -1,13 +1,13 @@
 // import childrens
-import SideBarBannerItems from './mains/SidebarBannerItems';
-import SideBarBrand from './mains/SidebarBrand';
-import SideBarButton from './mains/SidebarButton';
-import SideBarItems from './mains/SidebarItems';
+import SideBarBannerItems from "./mains/SidebarBannerItems";
+import SideBarBrand from "./mains/SidebarBrand";
+import SideBarButton from "./mains/SidebarButton";
+import SideBarItems from "./mains/SidebarItems";
 // Sidebar style
-import styles from './Sidebar.module.scss';
+import styles from "./Sidebar.module.scss";
 
 const Sidebar = ({ trans }) => (
-  <div className={styles['sidebar-container']}>
+  <div className={styles["sidebar-container"]}>
     <SideBarBrand />
     <SideBarItems trans={trans} />
     <SideBarBannerItems trans={trans} />
--- src/layouts/Sidebar/mains/SidebarBannerItems/index.jsx
+++ src/layouts/Sidebar/mains/SidebarBannerItems/index.jsx
@@ -1,11 +1,11 @@
 // f2 component
-import SidebarBannerItem from '../../components/SidebarBannerItem';
+import SidebarBannerItem from "../../components/SidebarBannerItem";
 // style
-import styles from './SidebarBannerItems.module.scss';
+import styles from "./SidebarBannerItems.module.scss";
 
 const SideBarBannerItems = ({ trans }) => (
-  <div className={styles['sidebar-banner-items-container']}>
-    {trans.sidebar['banner-items'].map((item) => (
+  <div className={styles["sidebar-banner-items-container"]}>
+    {trans.sidebar["banner-items"].map((item) => (
       <SidebarBannerItem key={item.title} item={item} />
     ))}
   </div>
