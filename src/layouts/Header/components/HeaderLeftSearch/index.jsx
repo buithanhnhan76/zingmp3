630b8249dce5341b33eef5ad188a0e9ca487c227
THANH NHAN BUI <thanhnhan.bui@m.citynow.vn>
2022-08-18 10:28:23 +0700

    optimize

--- src/layouts/Header/components/HeaderLeftSearch/index.jsx
+++ src/layouts/Header/components/HeaderLeftSearch/index.jsx
@@ -5,7 +5,7 @@ import styles from "./HeaderLeftSearch.module.scss";
 
 const HeaderLeftSearch = () => (
   <div className={styles["header-left-search-container"]}>
-    <SearchOutlined style={{fontSize: '16px', color: '#b3b2b6'}} />
+    <SearchOutlined style={{ fontSize: "16px", color: "#b3b2b6" }} />
     <input
       placeholder="Tìm kiếm bài hát, nghệ sĩ, lời bài hát..."
       className={styles["header-left-search"]}
