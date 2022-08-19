5d60ee9f12c330a35d9f06d0ff82222a31dbd710
thanhnhan.bui <thanhnhan.bui@m.citynow.vn>
2022-08-19 13:51:08 +0700

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,9 @@ import Head from "next/head";
 import React from "react";
 import iconZing from "public/icons/icon-zing.png";
 // multiple languages
+import HomeMain from "src/views/HomeMain";
 import useTranslate from "../hooks/useStranslate";
+// home main
 
 const Home = () => {
   const translate = useTranslate();
@@ -17,6 +19,7 @@ const Home = () => {
         <meta name="description" content="Zing Mp3" />
         <link rel="icon" href={iconZing.src} />
       </Head>
+      <HomeMain />
     </div>
   );
 };
