b2b17dadf2959315e62aeea3d6944ee3e6f06476
buithanhnhan76 <nhanbui37@gmail.com>
2022-08-19 14:30:05 +0700

    update folder structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,7 @@ import Head from "next/head";
 import React from "react";
 import iconZing from "public/icons/icon-zing.png";
 // multiple languages
-import HomeMain from "src/views/HomeMain";
 import useTranslate from "../hooks/useStranslate";
-// home main
 
 const Home = () => {
   const translate = useTranslate();
@@ -19,7 +17,6 @@ const Home = () => {
         <meta name="description" content="Zing Mp3" />
         <link rel="icon" href={iconZing.src} />
       </Head>
-      <HomeMain />
     </div>
   );
 };
