96430e56cdcf917ff5fc0c444df19f9b28cefbd7
buithanhnhan76 <nhanbui37@gmail.com>
2022-08-20 21:14:14 +0700

    update redux for sidebar and header

--- src/layouts/Header/components/SearchBar/index.jsx
+++ src/layouts/Header/components/SearchBar/index.jsx
@@ -1,16 +1,19 @@
 // libs
 import { SearchOutlined } from "@ant-design/icons";
+// redux
+import { useSelector } from "react-redux";
 // style
 import styles from "./SearchBar.module.scss";
 
-const SearchBar = () => (
-  <div className={styles["search-bar-container"]}>
+const SearchBar = () => {
+  const {translate} = useSelector((state) => state.locale);
+  return <div className={styles["search-bar-container"]}>
     <SearchOutlined style={{ fontSize: "16px", color: "#b3b2b6" }} />
     <input
-      placeholder="Tìm kiếm bài hát, nghệ sĩ, lời bài hát..."
+      placeholder={translate.header.searchBar.placeholder}
       className={styles["search-bar"]}
     />
   </div>
-);
+};
 
 export default SearchBar;
