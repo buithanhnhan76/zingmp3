999c3839dd23aa8fb7085d5af3a08849651d6226
thanhnhan.bui <thanhnhan.bui@m.citynow.vn>
2022-08-20 18:46:29 +0700

    add redux for sidebar

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,15 @@ import React from "react";
 import iconZing from "public/icons/icon-zing.png";
 // multiple languages
 import LayoutMain from "src/layouts/LayoutMain";
+// redux
+import { updateLocale } from "src/redux/actions/authenticationAction";
+import { useDispatch } from "react-redux";
 import useTranslate from "../hooks/useStranslate";
 
 const Home = () => {
   const translate = useTranslate();
+  const dispatch = useDispatch();
+  dispatch(updateLocale({ translate }));
 
   return (
     <div>
