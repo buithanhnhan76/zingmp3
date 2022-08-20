da0102c1506f48eacf41059b47c7690224f7e024
thanhnhan.bui <thanhnhan.bui@m.citynow.vn>
2022-08-20 16:43:46 +0700

    add redux to header

--- src/layouts/Header/components/LoginContainer/index.jsx
+++ src/layouts/Header/components/LoginContainer/index.jsx
@@ -2,12 +2,19 @@
 import LoginButton from "../LoginButton";
 import UserButton from "../UserButton";
 
-const LoginContainer = ({user}) => (
-  !user?<LoginButton />:<UserButton {...user}/>
-);
+import { store } from "src/redux/store";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
-LoginContainer.defaultProps = {
-  user: '',
-}
+const LoginContainer = () => {
+
+  const {authentication} = useSelector((state) => state);
+
+  return !Object.keys(authentication.user).length ? (
+    <LoginButton />
+  ) : (
+    <UserButton {...authentication.user} />
+  );
+};
 
 export default LoginContainer;
