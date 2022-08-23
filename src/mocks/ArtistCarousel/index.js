3743b79798973e904a360568b71236a93a934bca
THANH NHAN BUI <thanhnhan.bui@m.citynow.vn>
2022-08-23 17:44:36 +0700

    temp

---
+++ src/mocks/ArtistCarousel/index.js
@@ -0,0 +1,38 @@
+import chiDan from "public/photos/chi-dan.png";
+import trinhThanhBinh from "public/photos/trinh-thanh-binh.png";
+import mrSiro from "public/photos/mr-siro.png";
+
+export const listArtistCarousel = [
+  {
+    id: 1,
+    src: chiDan.src,
+  },
+  {
+    id: 2,
+    src: trinhThanhBinh.src,
+  },
+  {
+    id: 3,
+    src: mrSiro.src,
+  },
+  {
+    id: 4,
+    src: chiDan.src,
+  },
+  {
+    id: 5,
+    src: trinhThanhBinh.src,
+  },
+  {
+    id: 6,
+    src: mrSiro.src,
+  },
+  {
+    id: 7,
+    src: trinhThanhBinh.src,
+  },
+  {
+    id: 8,
+    src: mrSiro.src,
+  },
+];
