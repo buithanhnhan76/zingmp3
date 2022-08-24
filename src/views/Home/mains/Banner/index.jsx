da85d70693d25172c957354aef9dbe7724b3146e
buithanhnhan76 <nhanbui37@gmail.com>
2022-08-24 16:31:42 +0700

    fix banner component

--- src/views/Home/mains/Banner/index.jsx
+++ src/views/Home/mains/Banner/index.jsx
@@ -5,8 +5,10 @@ import BannerItems from "../../components/BannerItems";
 // style
 import styles from "./Banner.module.scss";
 
-const Banner = () => <div className={styles["banner-container"]}>
+const Banner = () => (
+  <div className={styles["banner-container"]}>
     <BannerItems listItems={listBannerItems} />
-</div>;
+  </div>
+);
 
 export default Banner;
