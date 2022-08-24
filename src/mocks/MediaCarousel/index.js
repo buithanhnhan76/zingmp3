078f4c533fd06d495526bcb5829541d401c22609
THANH NHAN BUI <thanhnhan.bui@m.citynow.vn>
2022-08-24 14:17:10 +0700

    complete media carousel

---
+++ src/mocks/MediaCarousel/index.js
@@ -0,0 +1,24 @@
+// photos
+import mediaOne from "public/photos/media-one.jpg";
+import mediaTwo from "public/photos/media-two.jpg";
+import mediaThree from "public/photos/media-three.jpg";
+import mediaFour from "public/photos/media-four.jpg";
+
+export const listMediaCarousel = [
+  {
+    id: 1,
+    src: mediaOne.src,
+  },
+  {
+    id: 2,
+    src: mediaTwo.src,
+  },
+  {
+    id: 3,
+    src: mediaThree.src,
+  },
+  {
+    id: 4,
+    src: mediaFour.src,
+  },
+];
---
+++ src/views/Home/mains/MediaCarousel/index.jsx
@@ -0,0 +1,32 @@
+// libs
+import Image from "next/image";
+import { Carousel } from "antd";
+import { useRef } from "react";
+import classNames from "classnames";
+// mock
+import { listMediaCarousel } from "src/mocks/MediaCarousel";
+// child
+import CarouselLeftButton from "../../components/CarouselLeftButton";
+import CarouselRightButton from "../../components/CarouselRightButton";
+// style
+import styles from "./MediaCarousel.module.scss";
+
+const MediaCarousel = () => {
+  const carousel = useRef();
+
+  return (
+    <div className={styles["carousel-container"]}>
+      <Carousel autoplay dots={false} slidesToShow={3} ref={carousel}>
+        {listMediaCarousel.map((item) => (
+          <div key={item.id} className={classNames(styles["carousel-item"])}>
+            <Image src={item.src} layout="fill" objectFit="cover" />
+          </div>
+        ))}
+      </Carousel>
+      <CarouselLeftButton carousel={carousel} />
+      <CarouselRightButton carousel={carousel} />
+    </div>
+  );
+};
+
+export default MediaCarousel;
