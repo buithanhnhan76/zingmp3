0bbc3d232c711dc15e63967d0e3ab20603c5574a
thanhnhan.bui <thanhnhan.bui@m.citynow.vn>
2022-08-25 20:32:22 +0700

    complete new release component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,12 +3,16 @@ import classNames from "classnames";
 // style
 import styles from "./Button.module.scss";
 
-const Button = ({ buttonLabel, borderColor, backGroundColor }) => (
+const Button = ({ buttonLabel, borderColor, backGroundColor, border, color, arrow }) => (
   <button
     className={classNames(
       styles["button-container"],
+      { [styles["border-none"]]: border === "none" },
       { [styles["border-white"]]: !borderColor },
-      { [styles["background-transparent"]]: !backGroundColor }
+      { [styles["background-transparent"]]: !backGroundColor },
+      { [styles["background-purple"]]: backGroundColor === "purple" },
+      { [styles["text-secondary"]]: color === "secondary" },
+      { [styles["button-arrow"]]: arrow === true }
     )}
   >
     {buttonLabel}
---
+++ src/views/Home/components/NewReleaseItems/index.jsx
@@ -0,0 +1,12 @@
+// child
+import NewReleaseItem from "../NewReleaseItem";
+// mock
+import { listNewReleaseSongs } from "src/mocks/NewRelease";
+// style
+import styles from "./NewReleaseItems.module.scss";
+
+const NewReleaseItems = () => <div className={styles["new-release-items-container"]}>
+    {listNewReleaseSongs.map(song => <NewReleaseItem id={song.id} item={song} />)}
+</div>
+
+export default NewReleaseItems;
