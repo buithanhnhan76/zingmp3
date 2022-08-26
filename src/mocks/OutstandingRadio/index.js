bf3cc901dbc81811fa54fae1326ae5b24ebdc6ac
THANH NHAN BUI <thanhnhan.bui@m.citynow.vn>
2022-08-26 10:12:51 +0700

    finish outstanding radio carousel

---
+++ src/mocks/OutstandingRadio/index.js
@@ -0,0 +1,63 @@
+// photos
+import radioPhotoOne from "public/photos/radio-photo-one.webp";
+import radioHostPhotoOne from "public/photos/radio-host-photo-one.webp";
+import LiveTag from "public/icons/live-tag.svg";
+
+export const listRadios = [
+  {
+    id: 1,
+    name: "Radio one",
+    listener: 300,
+    src: radioPhotoOne.src,
+    hostSrc: radioHostPhotoOne.src,
+    tagSrc: LiveTag.src,
+  },
+  {
+    id: 2,
+    name: "Radio two",
+    listener: 300,
+    src: radioPhotoOne.src,
+    hostSrc: radioHostPhotoOne.src,
+    tagSrc: LiveTag.src,
+  },
+  {
+    id: 3,
+    name: "Radio three",
+    listener: 300,
+    src: radioPhotoOne.src,
+    hostSrc: radioHostPhotoOne.src,
+    tagSrc: LiveTag.src,
+  },
+  {
+    id: 4,
+    name: "Radio four",
+    listener: 300,
+    src: radioPhotoOne.src,
+    hostSrc: radioHostPhotoOne.src,
+    tagSrc: LiveTag.src,
+  },
+  {
+    id: 5,
+    name: "Radio five",
+    listener: 300,
+    src: radioPhotoOne.src,
+    hostSrc: radioHostPhotoOne.src,
+    tagSrc: LiveTag.src,
+  },
+  {
+    id: 6,
+    name: "Radio six",
+    listener: 300,
+    src: radioPhotoOne.src,
+    hostSrc: radioHostPhotoOne.src,
+    tagSrc: LiveTag.src,
+  },
+  {
+    id: 7,
+    name: "Radio seven",
+    listener: 300,
+    src: radioPhotoOne.src,
+    hostSrc: radioHostPhotoOne.src,
+    tagSrc: LiveTag.src,
+  },
+];
