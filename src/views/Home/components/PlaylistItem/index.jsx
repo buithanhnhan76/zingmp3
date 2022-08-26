248e9b0428729a434af10ee6458144ec35831191
buithanhnhan76 <nhanbui37@gmail.com>
2022-08-26 11:57:00 +0700

    chore: have lunch

--- src/views/Home/components/PlaylistItem/index.jsx
+++ src/views/Home/components/PlaylistItem/index.jsx
@@ -3,9 +3,9 @@ import styles from "./PlaylistItem.module.scss";
 
 const PlaylistItem = ({ item }) => (
   <div className={styles["playlist-item-container"]}>
-    <Image src={item.src} width={195} height={195} objectFit="cover"/>
+    <Image src={item.src} width={195} height={195} objectFit="cover" className={styles["playlist-item-photo"]}/>
     <div className={styles["playlist-item-title"]}>{item.title}</div>
-    <div>{item.desc}</div>
+    <div className={styles["playlist-item-desc"]}>{item.desc}</div>
   </div>
 );
 
