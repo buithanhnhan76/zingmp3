c8bd3653fd434d7fc0fe438b7a1931c75e5e871c
thanhnhan.bui <thanhnhan.bui@m.citynow.vn>
2022-08-27 22:16:54 +0700

    new-song-carousel component

--- src/views/Home/components/PlaylistItem/index.jsx
+++ src/views/Home/components/PlaylistItem/index.jsx
@@ -3,7 +3,9 @@ import styles from "./PlaylistItem.module.scss";
 
 const PlaylistItem = ({ item }) => (
   <div className={styles["playlist-item-container"]}>
-    <Image src={item.src} width={195} height={195} objectFit="cover" className={styles["playlist-item-photo"]}/>
+    <div className={styles["playlist-item-photo"]}>
+      <Image src={item.src} layout="fill" objectFit="cover" />
+    </div>
     <div className={styles["playlist-item-title"]}>{item.title}</div>
     <div className={styles["playlist-item-desc"]}>{item.desc}</div>
   </div>
