2adfbcc1e891bb1e7d15417f6ba73c5675e396d0
buithanhnhan76 <nhanbui37@gmail.com>
2022-08-27 22:16:54 +0700

    new-song-carousel component

--- src/views/Home/components/PlaylistCarouselRanking/index.jsx
+++ src/views/Home/components/PlaylistCarouselRanking/index.jsx
@@ -1,6 +1,7 @@
 //libs
 import { Carousel } from "antd";
-import Image from "next/image";
+// component
+import PlaylistCarouselRankingItems from "../PlaylistCarouselRankingItems";
 // mocks
 import { listCarouselRankingSongs } from "src/mocks/PlaylistCarouselRanking";
 // style
@@ -14,34 +15,9 @@ const PlaylistCarouselRanking = () => (
       autoplay
       className={styles["carousel-container"]}
     >
-      {listCarouselRankingSongs.map((song) => (
-        <div>
-          <div
-            key={song.id}
-            className={styles["playlist-carousel-ranking-item"]}
-          >
-            <div className={styles["playlist-carousel-ranking-item-photo"]}>
-              <Image src={song.src} layout="fill" objectFit="cover"/>
-            </div>
-            <div className={styles["playlist-carousel-ranking-content"]}>
-              <div>
-                <h3 className={styles["playlist-carousel-ranking-item-title"]}>
-                  {song.title}
-                </h3>
-                <div
-                  className={styles["playlist-carousel-ranking-item-artists"]}
-                >
-                  {song.artists}
-                </div>
-              </div>
-              <div className={styles["playlist-carousel-ranking-item-content-footer"]}>
-                <div>{song.rank}</div>
-                <div> {song.dateRelease}</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
+      {listCarouselRankingSongs.map(
+        (song) => <PlaylistCarouselRankingItems key={song.id} song={song}/>
+      )}
     </Carousel>
   </div>
 );
