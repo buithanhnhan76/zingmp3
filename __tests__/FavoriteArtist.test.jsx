c2b12574f59ef4b7a54187f7b53e3f9aca391217
THANH NHAN BUI <thanhnhan.bui@m.citynow.vn>
2022-08-29 10:20:56 +0700

    set up jest, add a basic test case and fix header issue in large screen

---
+++ __tests__/FavoriteArtist.test.jsx
@@ -0,0 +1,17 @@
+// __tests__/index.test.jsx
+
+import { render, screen } from "@testing-library/react";
+import FavoriteArtist from "src/views/Home/mains/FavoriteArtist";
+import "@testing-library/jest-dom";
+
+describe("Favorite Artist component", () => {
+  it("renders Favorite Artist component", () => {
+    render(<FavoriteArtist />);
+
+    const heading = screen.getByRole("heading", {
+      name: /Nghệ Sĩ Yêu Thích/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
