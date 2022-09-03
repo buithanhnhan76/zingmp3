525c1a2af8729959b3700d341f30f16bef5e9ea8
thanhnhan.bui <thanhnhan.bui@m.citynow.vn>
2022-09-03 11:26:27 +0700

    move main layout to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,16 @@
 import { wrapper, store } from "src/redux/store";
 import { Provider } from "react-redux";
 import "antd/dist/antd.css";
+// layout
+import MainLayout from "src/layouts/MainLayout";
 // style
 import "styles/globals.css";
 
 const MyApp = ({ Component, pageProps }) => (
   <Provider store={store}>
-    <Component {...pageProps} />
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
   </Provider>
 );
 
