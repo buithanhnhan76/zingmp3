4b7a503b51f2db770e9ab6788eee55a6fa5af1ad
THANH NHAN BUI <thanhnhan.bui@m.citynow.vn>
2022-09-15 14:16:58 +0700

    update setting item icons

--- src/mocks/Header/HeaderMockData.js
+++ src/mocks/Header/HeaderMockData.js
@@ -1,10 +1,12 @@
-import Clip from "public/icons/clip.svg";
+import DiscountLabelPrice from "public/icons/discount-label-price.svg";
+import DiscountHotSale from "public/icons/discount-hot-sale.svg";
+import DiscountLabelSale from "public/icons/discount-label-sale.svg";
 
 const listSettingItems = [
-  { id: 1, src: Clip.src },
-  { id: 2, src: Clip.src },
-  { id: 3, src: Clip.src },
-  { id: 4, src: Clip.src },
+  { id: 1, src: DiscountHotSale.src },
+  { id: 2, src: DiscountLabelPrice.src },
+  { id: 3, src: DiscountLabelSale.src },
+  { id: 4, src: DiscountHotSale.src },
 ];
 
 export { listSettingItems };
