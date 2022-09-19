ee4b7821aedfb996dabe71a85fdde92e291d58c8
thanhnhan.bui <thanhnhan.bui@m.citynow.vn>
2022-09-19 15:13:46 +0700

    add aaa pattern to cypress test cases

--- cypress/e2e/zing-mp3-test/side-bar-item.cy.js
+++ cypress/e2e/zing-mp3-test/side-bar-item.cy.js
@@ -1,16 +1,22 @@
 /// <reference types="cypress" />
+// utils
+import { getDataCy } from "../utils";
 
-describe("sidebar item test", () => {
-  it("sidebar item click test", () => {
-    cy.visit("/");
-
-    cy.get('*[data-cy^="sidebar-category-item"]').first().click();
-
-    cy.get('*[data-cy^="sidebar-category-item"]')
+describe("Sidebar item test", () => {
+  // varibles
+  const baseUrl = "/";
+  const sidebarCategoryItem = "sidebar-category-item";
+  const sideBarItemActive = "sidebar-item-active";
+  it("Sidebar item click", () => {
+    // Action
+    cy.visit(baseUrl);
+    cy.get(getDataCy(sidebarCategoryItem)).first().click();
+    // Assert
+    cy.get(getDataCy(sidebarCategoryItem))
       .first()
       .should(($div) => {
         const classes = $div.attr("class");
-        expect(classes).to.contain("sidebar-item-active");
+        expect(classes).to.contain(sideBarItemActive);
       });
   });
 });
