9c6000da395299e942456bd80d86c5e417948419
buithanhnhan76 <nhanbui37@gmail.com>
2023-02-04 23:24:08 +0700

    refactor BannerItem component

---
+++ src/constant/index.js
@@ -0,0 +1,3 @@
+const VIPBANNER = "vip-banner";
+
+export default { VIPBANNER };
--- src/layouts/MainLayout/components/BannerItem/index.jsx
+++ src/layouts/MainLayout/components/BannerItem/index.jsx
@@ -1,13 +1,22 @@
-// vip banner
-import VipBanner from "../VipBanner";
-// login banner
-import LoginBanner from "../LoginBanner";
+// constant
+import constant from "src/constant";
+// style
+import styles from "./BannerItem.module.scss";
 
-const BannerItem = ({ item }) =>
-  item.type === "vip-banner" ? (
-    <VipBanner item={item} />
-  ) : (
-    <LoginBanner item={item} />
-  );
-
+const BannerItem = ({ item }) => {
+  let bannerContainerClassName =
+    item.type === constant.VIPBANNER ? "vip-banner-container" : "login-banner-container";
+  let bannerButtonClassName =
+    item.type === constant.VIPBANNER
+      ? "vip-banner-button"
+      : "login-banner-button";
+  return (
+    <div className={styles[bannerContainerClassName]}>
+      <h3>{item.title}</h3>
+      <button type="button" className={styles[bannerButtonClassName]}>
+        {item.buttonLabel}
+      </button>
+    </div>
+  )
+};
 export default BannerItem;
