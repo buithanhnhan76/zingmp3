07b4256fae2d31cd40e5d2a066def0728bf19ffe
buithanhnhan76 <nhanbui37@gmail.com>
2023-02-18 15:21:00 +0700

    update remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,5 @@
 // remix.config.js
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  serverDependenciesToBundle: [
-    /@ant-design\/*/,
-    /@antv\/*/,
-    /d3-interpolate/,
-    /d3-color/,
-    /lodash-es/,
-  ],
+  serverDependenciesToBundle: ["d3-interpolate"],
 };
