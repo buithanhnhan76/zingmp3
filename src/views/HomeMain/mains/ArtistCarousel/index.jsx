a1176c8c9786edd0086c6e6f32543376ac56045d
buithanhnhan76 <nhanbui37@gmail.com>
2023-02-21 09:46:29 +0700

    update ArtistCarousel component

--- src/views/HomeMain/mains/ArtistCarousel/index.jsx
+++ src/views/HomeMain/mains/ArtistCarousel/index.jsx
@@ -4,6 +4,8 @@ import { Carousel } from "antd";
 import { useRef } from "react";
 // mock
 import { listArtistCarousel } from "src/mocks/ArtistCarousel";
+// hook
+import { useSlidesNumber } from "src/hooks/useSlidesNumber";
 // children
 import LeftButton from "../../components/LeftButton";
 import RightButton from "../../components/RightButton";
@@ -12,11 +14,12 @@ import styles from "./ArtistCarousel.module.scss";
 
 const ArtistCarousel = () => {
   const carousel = useRef();
-
+  const numberOfSlidesToShow = useSlidesNumber(7, 5, 3, 1);
   return (
     <div className={styles["carousel-container"]}>
+      {console.log(numberOfSlidesToShow)}
       <LeftButton carousel={carousel} />
-      <Carousel autoplay dots={false} slidesToShow={7} ref={carousel}>
+      <Carousel autoplay dots={true} slidesToShow={numberOfSlidesToShow} ref={carousel}>
         {listArtistCarousel.map((item) => (
           <div key={item.id} className={styles["carousel-item"]}>
             <Image src={item.src} layout="fill" />
