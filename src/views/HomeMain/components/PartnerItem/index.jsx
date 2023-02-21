d7a0ee893ced0f88716bed681ffcc67f5b86faa3
buithanhnhan76 <nhanbui37@gmail.com>
2023-02-21 20:34:35 +0700

    update and refactor Partner component

--- src/views/HomeMain/components/PartnerItem/index.jsx
+++ src/views/HomeMain/components/PartnerItem/index.jsx
@@ -1,17 +1,17 @@
+// libs
 import Image from "next/legacy/image";
 // style
 import styles from "./PartnerItem.module.scss";
 
 const PartnerItem = ({ item }) => (
-  <div className={styles["partner-item-container"]}>
+  <figure className={styles["partner-item-container"]}>
     <Image
       width={90}
       height={50}
       src={item.src}
       alt="Partner Icon"
-      className={styles["partner-item-image"]}
     />
-  </div>
+  </figure>
 );
 
 export default PartnerItem;
