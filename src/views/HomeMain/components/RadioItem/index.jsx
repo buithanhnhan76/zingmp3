458172858e75205dddc0a8f81af40ad026450992
buithanhnhan76 <nhanbui37@gmail.com>
2023-02-22 15:31:34 +0700

    add pointer class to components

--- src/views/HomeMain/components/RadioItem/index.jsx
+++ src/views/HomeMain/components/RadioItem/index.jsx
@@ -1,9 +1,11 @@
 // children
 import RadioItemTop from "../RadioItemTop";
 import RadioItemBottom from "../RadioItemBottom";
+// styles
+import styles from "./RadioItem.module.scss";
 
 const RadioItem = ({ item }) => (
-  <div>
+  <div class={styles["radio-item"]}>
     <RadioItemTop item={item} />
     <RadioItemBottom item={item} />
   </div>
