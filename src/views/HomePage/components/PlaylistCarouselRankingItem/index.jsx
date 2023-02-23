4fba82deca7782542ab2999c151159fcad6e7efc
buithanhnhan76 <nhanbui37@gmail.com>
2023-02-23 19:45:07 +0700

    fix PlaylistCarousel Ranking and Item

--- src/views/HomePage/components/PlaylistCarouselRankingItem/index.jsx
+++ src/views/HomePage/components/PlaylistCarouselRankingItem/index.jsx
@@ -4,26 +4,22 @@ import Image from "next/legacy/image";
 import styles from "./PlaylistCarouselRankingItem.module.scss";
 
 const PlaylistCarouselRankingItem = ({ song, order }) => (
-  <div key={song.id} className={styles["playlist-carousel-ranking-item-container"]} order={order}>
-    <div className={styles["playlist-carousel-ranking-item"]}>
-      <div className={styles["playlist-carousel-ranking-item-photo"]}>
-        <Image src={song.src} layout="fill" objectFit="cover" />
+  <div className={styles["playlist-carousel-ranking-item"]} order={order}>
+    <div className={styles["playlist-carousel-ranking-item-photo"]}>
+      <Image src={song.src} layout="fill" objectFit="cover" />
+    </div>
+    <div className={styles["playlist-carousel-ranking-content"]}>
+      <div>
+        <h3 className={styles["playlist-carousel-ranking-item-title"]}>
+          {song.title}
+        </h3>
+        <div className={styles["playlist-carousel-ranking-item-artists"]}>
+          {song.artists}
+        </div>
       </div>
-      <div className={styles["playlist-carousel-ranking-content"]}>
-        <div>
-          <h3 className={styles["playlist-carousel-ranking-item-title"]}>
-            {song.title}
-          </h3>
-          <div className={styles["playlist-carousel-ranking-item-artists"]}>
-            {song.artists}
-          </div>
-        </div>
-        <div
-          className={styles["playlist-carousel-ranking-item-content-footer"]}
-        >
-          <div>{song.rank}</div>
-          <div> {song.dateRelease}</div>
-        </div>
+      <div className={styles["playlist-carousel-ranking-item-content-footer"]}>
+        <div>{song.rank}</div>
+        <div> {song.dateRelease}</div>
       </div>
     </div>
   </div>
