6a968ec5a97071c395bbcc3296f449d4b022d400
buithanhnhan76 <nhanbui37@gmail.com>
2023-02-23 15:51:10 +0700

    update and fix HistoryButtons

--- src/layouts/MainLayout/components/HistoryButtons/index.jsx
+++ src/layouts/MainLayout/components/HistoryButtons/index.jsx
@@ -1,8 +1,15 @@
+// children
+import Button from "src/components/Button";
 // styles
 import styles from "./HistoryButtons.module.scss";
 
 const HistoryButtons = () => (
-  <div className={styles["history-buttons-container"]} />
+  <div className={styles["history-buttons-container"]}>
+    {/* <Button largeLeftArrow />
+    <Button largeRightArrow /> */}
+    <button className={styles["previous-button"]} />
+    <button className={styles["next-button"]} /> 
+  </div>
 );
 
 export default HistoryButtons;
