b380970d6b458c649e01fc5eda6addf7e37f6714
buithanhnhan76 <nhanbui37@gmail.com>
2023-02-23 10:09:53 +0700

    update meta tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,6 +18,25 @@ const Home = () => {
         <title>{title}</title>
         <meta name="description" content="Zing Mp3" />
         <link rel="icon" href={iconZing.src} />
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="keywords" content="zing, zingmp3, mp3" />
+        <link rel="author" href="https://www.buithanhnhan.xyz" />
+        <link rel="canonical" href="https://www.buithanhnhan.xyz" />
+        <meta property="og:url" content="https://zingmp3-gold.vercel.app/en" />
+        {/* <meta property="og:image" content={url("src/pho")} /> */}
+        <meta
+          property="og:description"
+          content="Zing mp3, nghe nhac, tai nhac "
+        />
+        <meta property="og:title" content="Zing mp3" />
+        <meta property="og:site_name" content="Zing mp3" />
+        <meta property="og:see_also" content="https://www.buithanhnhan.xyz" />
+        <meta name="twitter:card" content="Zing mp3" />
+        <meta name="twitter:url" content="https://zingmp3-gold.vercel.app/en" />
+        <meta name="twitter:title" content="Zing mp3" />
+        <meta name="twitter:description" content="Zing mp3" />
+        <meta name="twitter:image" href={iconZing.src} />
       </Head>
       <HomeMain />
     </div>
