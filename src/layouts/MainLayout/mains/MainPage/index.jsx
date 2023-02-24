85886d3d50bc049fa69f94bcabf46271e3d36ac3
buithanhnhan76 <nhanbui37@gmail.com>
2023-02-24 14:15:58 +0700

    update MainPage class name

--- src/layouts/MainLayout/mains/MainPage/index.jsx
+++ src/layouts/MainLayout/mains/MainPage/index.jsx
@@ -6,7 +6,7 @@ import Header from "../../components/Header";
 import styles from "./MainPage.module.scss";
 
 const MainPage = ({ children }) => (
-  <div className={styles.mainpage}>
+  <div className={styles["main-page"]}>
     <Header />
     {children}
   </div>
