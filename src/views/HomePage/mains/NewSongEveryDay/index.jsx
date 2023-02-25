3f5caf8a515c03504a9219330103c92ce620c169
buithanhnhan76 <nhanbui37@gmail.com>
2023-02-25 20:29:48 +0700

    update home components spacing and move playlist to mains

--- src/views/HomePage/mains/NewSongEveryDay/index.jsx
+++ src/views/HomePage/mains/NewSongEveryDay/index.jsx
@@ -3,16 +3,16 @@ import { useSelector } from "react-redux";
 // mock
 import { listPlaylistItems } from "src/mocks/Playlist";
 // child
-import Playlist from "../../components/Playlist";
+import Playlist from "../Playlist";
 
 const NewSongEveryDay = () => {
   const title = useSelector(
     (state) => state.locale.translate.newSongEveryday.title
   );
   return (
-    <section>
+    <>
       <Playlist title={title} listItems={listPlaylistItems} />
-    </section>
+    </>
   );
 };
 
--- src/views/HomePage/mains/Playlist/index.jsx
+++ src/views/HomePage/mains/Playlist/index.jsx
@@ -1,12 +1,12 @@
-import PlaylistItems from "../PlaylistItems";
+import PlaylistItems from "../../components/PlaylistItems";
 // style
 import styles from "./Playlist.module.scss";
 
 const Playlist = ({ title, listItems }) => (
-  <div className={styles["playlist-container"]}>
+  <section className={styles["playlist-container"]}>
     <h3 className={styles["playlist-title"]}>{title}</h3>
     <PlaylistItems listItems={listItems} />
-  </div>
+  </section>
 );
 
 export default Playlist;
