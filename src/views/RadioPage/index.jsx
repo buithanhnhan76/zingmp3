9ffca9a253a2231eb5b8642a433c00758b28ddd8
buithanhnhan76 <nhanbui37@gmail.com>
2023-02-25 14:53:02 +0700

    add pages

---
+++ src/views/RadioPage/index.jsx
@@ -0,0 +1,10 @@
+// styles
+import styles from "./RadioPage.module.scss";
+
+const RadioPage = () => (
+  <main className={styles["radio-page"]}>
+    <h3>Radio Page</h3>
+  </main>
+);
+
+export default RadioPage;
---
+++ src/views/ZingChartPage/index.jsx
@@ -0,0 +1,10 @@
+// styles
+import styles from "./ZingChartPage.module.scss";
+
+const ZingChartPage = () => (
+  <main className={styles["zing-chart-page"]}>
+    <h3>Zingchart Page</h3>
+  </main>
+);
+
+export default ZingChartPage;
