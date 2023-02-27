5c49a91ef5706b30a23456b5afe1025f8e4d5ef1
buithanhnhan76 <nhanbui37@gmail.com>
2023-02-27 15:54:06 +0700

    update SectionTitle in NewSongCarouselTitle and Playlist

--- src/views/HomePage/mains/Playlist/index.jsx
+++ src/views/HomePage/mains/Playlist/index.jsx
@@ -1,10 +1,12 @@
+// children
+import SectionTitle from "src/components/SectionTitle";
 import PlaylistItems from "../../components/PlaylistItems";
 // style
 import styles from "./Playlist.module.scss";
 
 const Playlist = ({ title, listItems }) => (
   <section className={styles["playlist-container"]}>
-    <h3 className={styles["playlist-title"]}>{title}</h3>
+    <SectionTitle>{title}</SectionTitle>
     <PlaylistItems listItems={listItems} />
   </section>
 );
