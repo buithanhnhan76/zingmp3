9317e1584b613952eaa61dc256c1a3749b8de1f2
buithanhnhan76 <nhanbui37@gmail.com>
2023-03-01 19:31:04 +0700

    refactor dynamic search bar

--- src/layouts/MainLayout/components/HeaderLeft/index.jsx
+++ src/layouts/MainLayout/components/HeaderLeft/index.jsx
@@ -1,13 +1,13 @@
 // children
 import HistoryButton from "../HistoryButtons";
-import SearchBar from "../SearchBar";
+import DynamicSearchBar from "src/components/DynamicSearchBar";
 // style
 import styles from "./HeaderLeft.module.scss";
 
 const HeaderLeft = () => (
   <div className={styles["left-side-header-container"]}>
     <HistoryButton />
-    <SearchBar />
+    <DynamicSearchBar />
   </div>
 );
 
